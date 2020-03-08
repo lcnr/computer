c17a7404fc1218e7d644d55927f6c1666b957398
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-03-08 23:47:55 +0100

    update coloring

--- boulder/lir/src/optimize/coloring.rs
+++ boulder/lir/src/optimize/coloring.rs
@@ -1,6 +1,6 @@
 use graphc::{Coloring, Graph, NodeId};
 
-use tindex::TBitSet;
+use tindex::{TBitSet, TVec};
 
 use shared_id::LocationId;
 
@@ -54,33 +54,51 @@ impl Block {
 
         // lir reuses locations, this makes the graph algorithm less efficent,
         // to solve this, we make each living location unique
-        //let mut replacements: TVec<LocationId, LocationId> = (0..).map(LocationId).take(self.memory_len).collect();
-        //let mut repl = (self.memory_len..).map(LocationId);
+        let mut replacements: TVec<LocationId, LocationId> =
+            (0..).map(LocationId).take(self.memory_len).collect();
+        let mut repl = (self.memory_len..).map(LocationId);
 
         for _ in 0..self.memory_len {
             g.add_node();
         }
 
-        let mut add_alive = |alive: &mut TBitSet<LocationId>, n: LocationId| {
+        fn add_alive(g: &mut Graph, alive: &mut TBitSet<LocationId>, n: LocationId) {
             for other in alive.iter() {
                 g.add_edge(NodeId::from(n.0), NodeId::from(other.0));
             }
             alive.add(n);
-        };
-
-        self.terminator.reads(|id| add_alive(&mut alive, id));
-
-        for step in self.steps.iter().rev() {
-            step.writes(|id| {
-                add_alive(&mut alive, id);
-                alive.remove(id);
-            });
-            step.reads(|id| add_alive(&mut alive, id));
         }
 
-        for &input in self.inputs.iter().rev() {
-            add_alive(&mut alive, input);
-            alive.remove(input)
+        self.terminator
+            .reads(|id| add_alive(&mut g, &mut alive, id));
+
+        for step in self.steps.iter_mut().rev() {
+            step.writes(|id| {
+                let updated = replacements[id];
+                add_alive(&mut g, &mut alive, updated);
+                alive.remove(updated);
+
+                g.add_node();
+                replacements[id] = repl.next().unwrap();
+
+                updated
+            });
+            step.reads(|id| {
+                let updated = replacements[id];
+                add_alive(&mut g, &mut alive, updated);
+                updated
+            });
+        }
+
+        for id in self.inputs.iter_mut().rev() {
+            let updated = replacements[*id];
+            add_alive(&mut g, &mut alive, updated);
+            alive.remove(updated);
+
+            g.add_node();
+            replacements[*id] = repl.next().unwrap();
+
+            *id = updated;
         }
 
         g.minimal_coloring()
